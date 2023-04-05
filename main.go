7496e29b55488b42bba97c469a4226784b5ad1f3
github user tmcoco0228si <proguteinitar3150@gmail.com>
2023-04-05 23:55:37 +0900

    リファクタリング

--- main.go
+++ main.go
@@ -5,57 +5,29 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"net/http"
 	"os"
-	"os/signal"
-	"syscall"
-	"time"
 
 	"github.com/trailstem/go_task_app/config"
-	"golang.org/x/sync/errgroup"
 )
 
 // func run(ctx context.Context, l net.Listener) error {
 func run(ctx context.Context) error {
 
-	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
-	defer stop()
-
 	cfg, err := config.New()
+
 	if err != nil {
 		return err
 	}
-
 	l, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.Port))
-
 	if err != nil {
-		log.Fatalf("failed to listen port %d : %v", cfg.Port, err)
+		log.Fatalf("failed to listen port %d: %v", cfg.Port, err)
 	}
-
 	url := fmt.Sprintf("http://%s", l.Addr().String())
 	log.Printf("start with: %v", url)
 
-	//サーバ設定
-	s := &http.Server{
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			time.Sleep(5 * time.Second)
-			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
-		}),
-	}
-
-	eg, ctx := errgroup.WithContext(ctx)
-	eg.Go(func() error {
-		if err := s.Serve(l); err != nil && err != http.ErrServerClosed {
-			log.Printf("failed to close: %v", err)
-		}
-		return nil
-	})
-
-	<-ctx.Done()
-	if err := s.Shutdown(context.Background()); err != nil {
-		log.Printf("failed to shutdown: %+v", err)
-	}
-	return eg.Wait()
+	mux := NewMux()
+	s := NewServer(l, mux)
+	return s.Run(ctx)
 }
 
 func main() {
