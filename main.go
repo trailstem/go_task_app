0f88fad70b77eee7069ca2fdbc85ba08d45f93ee
github user tmcoco0228si <proguteinitar3150@gmail.com>
2023-04-05 23:40:38 +0900

    ルーティング定義したmux

--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/trailstem/go_task_app/config"
 	"golang.org/x/sync/errgroup"
@@ -14,6 +17,9 @@ import (
 
 // func run(ctx context.Context, l net.Listener) error {
 func run(ctx context.Context) error {
+
+	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
+	defer stop()
 
 	cfg, err := config.New()
 	if err != nil {
@@ -32,6 +38,7 @@ func run(ctx context.Context) error {
 	//サーバ設定
 	s := &http.Server{
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			time.Sleep(5 * time.Second)
 			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 		}),
 	}
