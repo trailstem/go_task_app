533f4b9f2fc38cef91061e6345ce984f40d9cb3d
github user tmcoco0228si <proguteinitar3150@gmail.com>
2023-04-09 12:41:11 +0900

    HTTPハンドラ→永続化

--- mux.go
+++ mux.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"context"
 	"net/http"
 
 	"github.com/go-chi/chi"
 	"github.com/go-playground/validator"
+	"github.com/trailstem/go_task_app/clock"
+	"github.com/trailstem/go_task_app/config"
 	"github.com/trailstem/go_task_app/handler"
 	"github.com/trailstem/go_task_app/store"
 )
 
-func NewMux() http.Handler {
+func NewMux(ctx context.Context, cfg *config.Config) (http.Handler, func(), error) {
 	mux := chi.NewRouter()
 	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json; charset=utf-8")
@@ -17,10 +20,17 @@ func NewMux() http.Handler {
 	})
 
 	v := validator.New()
-	at := &handler.AddTask{Store: store.Tasks, Validator: v}
+	db, cleanup, err := store.New(ctx, cfg)
+
+	if err != nil {
+		return nil, cleanup, err
+	}
+
+	r := store.Repository{Clocker: clock.RealClocker{}}
+	at := &handler.AddTask{DB: db, Repo: &r, Validator: v}
 
 	mux.Post("/tasks", at.ServeHTTP)
-	lt := &handler.ListTask{Store: store.Tasks}
+	lt := &handler.ListTask{DB: db, Repo: r}
 	mux.Get("/tasks", lt.ServeHTTP)
-	return mux
+	return mux, cleanup, nil
 }
